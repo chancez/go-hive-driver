4e738003c441df8409a270205799a591947c5be2
Chance Zibolski <czibolsk@redhat.com>
2019-05-15 17:00:01 -0700

    Support DialTimeout for TLS connections

--- dial.go
+++ dial.go
@@ -41,9 +41,29 @@ func (d DialWrapper) DialContext(ctx context.Context, network, address string) (
 }
 
 type TLSDialer struct {
+	dialer net.Dialer
 	*tls.Config
 }
 
 func (d TLSDialer) Dial(network, address string) (net.Conn, error) {
-	return tls.Dial(network, address, d.Config)
+	return tls.DialWithDialer(&d.dialer, network, address, d.Config)
+}
+
+func (d TLSDialer) DialTimeout(network, address string, timeout time.Duration) (net.Conn, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	return d.DialContext(ctx, network, address)
+}
+
+func (d TLSDialer) DialContext(ctx context.Context, network, address string) (net.Conn, error) {
+	conn, err := d.dialer.DialContext(ctx, network, address)
+	if err != nil {
+		return nil, err
+	}
+	tlsConn := tls.Client(conn, d.Config)
+	err = tlsConn.Handshake()
+	if err != nil {
+		return nil, err
+	}
+	return tlsConn, nil
 }
